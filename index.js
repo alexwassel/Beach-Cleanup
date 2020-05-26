bd4adc30c6c624d44ed41cbc3f63e78cbbc513f2
Alex Wassel <awassel18@gmail.com>
2020-05-25 19:11:34 -0700

    switched to docs

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = 3000;
 const request = require('request')
 const fs = require("fs");
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'docs/index.html'));
 })
 
 app.listen(port, () => console.log(`Digital Ocean app listening on port ${port}!`))
